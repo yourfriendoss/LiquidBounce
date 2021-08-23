ef5b29864ffce606ad85e78182176c44be970a39
memsss <aggelos.bilai73@gmail.com>
2021-08-23 16:48:26 +0300

    tests

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/world/ModuleLiquidInteract.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/world/ModuleLiquidInteract.kt
@@ -3,6 +3,6 @@ package net.ccbluex.liquidbounce.features.module.modules.world
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
 
-object ModuleLiquidInteract : Module("Ignite", Category.WORLD) {
+object ModuleLiquidInteract : Module("LiquidInteract", Category.WORLD) {
     val a by boolean("a", false)
 }
