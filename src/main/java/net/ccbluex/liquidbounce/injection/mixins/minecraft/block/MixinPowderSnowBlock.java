945db33269d8333fe908306177a5919fdc6b5b92
memsss <aggelos.bilai73@gmail.com>
2021-08-27 14:30:59 +0300

    tests

--- src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/block/MixinPowderSnowBlock.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/block/MixinPowderSnowBlock.java
@@ -18,7 +18,7 @@ public class MixinPowderSnowBlock {
     @Inject(method = "onEntityCollision", at = @At("HEAD"), cancellable = true)
     private void hookEntityCollision(BlockState state, World world, BlockPos pos, Entity entity, CallbackInfo ci) {
         if (ModuleNoSlow.INSTANCE.getEnabled() && ModuleNoSlow.PowderSnow.INSTANCE.getEnabled()) {
-            entity.slowMovement(state, new Vec3d(0.0, 0.0, 0.0));
+            ci.cancel();
             if (ModuleNoSlow.PowderSnow.INSTANCE.getA()) entity.setVelocity(ModuleNoSlow.PowderSnow.INSTANCE.getMultiplier(), entity.getVelocity().y, ModuleNoSlow.PowderSnow.INSTANCE.getMultiplier());
         }
     }
