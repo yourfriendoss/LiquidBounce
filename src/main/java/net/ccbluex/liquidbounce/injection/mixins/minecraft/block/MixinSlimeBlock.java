b4fc5293d9f787246f9db9ec9818af55c2eb7e2c
memsss <aggelos.bilai73@gmail.com>
2021-08-27 14:40:03 +0300

    tests

--- src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/block/MixinSlimeBlock.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/block/MixinSlimeBlock.java
@@ -14,11 +14,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(SlimeBlock.class)
 public class MixinSlimeBlock {
 
+    @Inject(method = "bounce", at = @At("HEAD"), cancellable = true)
+    private void hookBounce(Entity entity, CallbackInfo ci) {
+        if (ModuleNoSlow.INSTANCE.getEnabled() && ModuleNoSlow.Slime.INSTANCE.getEnabled()) {
+            ci.cancel();
+        }
+    }
+
     @Inject(method = "onSteppedOn", at = @At("HEAD"), cancellable = true)
     private void hookStep(World world, BlockPos pos, BlockState state, Entity entity, CallbackInfo ci) {
         if (ModuleNoSlow.INSTANCE.getEnabled() && ModuleNoSlow.Slime.INSTANCE.getEnabled()) {
             ci.cancel();
-            entity.setVelocity(entity.getVelocity().x * ModuleNoSlow.PowderSnow.INSTANCE.getMultiplier(), entity.getVelocity().y, entity.getVelocity().z * ModuleNoSlow.PowderSnow.INSTANCE.getMultiplier());
+            entity.setVelocity(entity.getVelocity().multiply(ModuleNoSlow.PowderSnow.INSTANCE.getMultiplier(), entity.getVelocity().y, ModuleNoSlow.PowderSnow.INSTANCE.getMultiplier()));
         }
     }
 }
