23c9e12c71233ce3cb6aeb13bd3b3f3df1642ca4
memsss <aggelos.bilai73@gmail.com>
2021-08-27 15:13:24 +0300

    tests

--- src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/block/MixinSlimeBlock.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/block/MixinSlimeBlock.java
@@ -14,8 +14,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(SlimeBlock.class)
 public class MixinSlimeBlock {
 
-    @Inject(method = "onSteppedOn", at = @At("HEAD"), cancellable = true)
-    private void hookStep(World world, BlockPos pos, BlockState state, Entity entity, CallbackInfo ci) {
+    @Inject(method = "bounce", at = @At("HEAD"), cancellable = true)
+    private void hookBounce(Entity entity, CallbackInfo ci) {
         if (ModuleNoSlow.INSTANCE.getEnabled() && ModuleNoSlow.Slime.INSTANCE.getEnabled()) {
             ci.cancel();
         }
