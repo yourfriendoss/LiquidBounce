58f650df932dd48f9d9b58566fea52ab1669ae7a
memsss <aggelos.bilai73@gmail.com>
2021-08-27 14:55:58 +0300

    tests

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/ModuleNoSlow.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/ModuleNoSlow.kt
@@ -86,6 +86,18 @@ object ModuleNoSlow : Module("NoSlow", Category.MOVEMENT) {
 
     object Slime : ToggleableConfigurable(this, "SlimeBlock", true) {
         val multiplier by float("Multiplier", 1f, 0.4f..2f)
+
+        val blockSlipperinessMultiplierHandler = handler<BlockSlipperinessMultiplierEvent> { event ->
+            if (event.block is SlimeBlock) {
+                event.slipperiness = 0.6f
+            }
+        }
+
+        val blockVelocityHandler = handler<BlockVelocityMultiplierEvent> { event ->
+            if (event.block is SlimeBlock) {
+                event.multiplier = multiplier
+            }
+        }
     }
 
     private object Honey : ToggleableConfigurable(this, "HoneyBlock", true) {
