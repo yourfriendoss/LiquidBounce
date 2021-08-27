8be0a681c35aa77cc814eedbb7aa8e76b624063e
memsss <aggelos.bilai73@gmail.com>
2021-08-27 16:24:19 +0300

    tests

--- src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/block/MixinSlimeBlock.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/block/MixinSlimeBlock.java
@@ -4,28 +4,32 @@ import net.ccbluex.liquidbounce.features.module.modules.movement.ModuleNoSlow;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SlimeBlock;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(SlimeBlock.class)
 public class MixinSlimeBlock {
-
-    @Inject(method = "bounce", at = @At("HEAD"), cancellable = true)
-    private void hookBounce(Entity entity, CallbackInfo ci) {
-        if (ModuleNoSlow.INSTANCE.getEnabled() && ModuleNoSlow.Slime.INSTANCE.getEnabled()) {
-            ci.cancel();
+    /**
+     * @author mems01
+     */
+    @Overwrite
+    private void bounce(Entity entity) {
+        Vec3d vec3d = entity.getVelocity();
+        if (ModuleNoSlow.INSTANCE.getEnabled() && ModuleNoSlow.Slime.INSTANCE.getEnabled() ) {
+            entity.setVelocity(vec3d);
+            return;
         }
-    }
-
-    @Inject(method = "onSteppedOn", at = @At("HEAD"), cancellable = true)
-    private void hookStep(World world, BlockPos pos, BlockState state, Entity entity, CallbackInfo ci) {
-        if (ModuleNoSlow.INSTANCE.getEnabled() && ModuleNoSlow.Slime.INSTANCE.getEnabled()) {
-            ci.cancel();
-            entity.getVelocity().y *= 1.0;
+        if (vec3d.y < 0.0D) {
+            double d = entity instanceof LivingEntity ? 1.0D : 0.8D;
+            entity.setVelocity(vec3d.x, -vec3d.y * d, vec3d.z);
         }
+
     }
 }
