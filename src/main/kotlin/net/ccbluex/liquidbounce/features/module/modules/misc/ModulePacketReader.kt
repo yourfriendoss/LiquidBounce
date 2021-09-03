9ccec2dec80e4434d9863332b59d36c2c8bf51dd
memsss <aggelos.bilai73@gmail.com>
2021-09-03 03:30:51 +0300

    tests

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModulePacketReader.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModulePacketReader.kt
@@ -1,18 +1,14 @@
 package net.ccbluex.liquidbounce.features.module.modules.misc
 
-import net.ccbluex.liquidbounce.event.PacketEvent
-import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.event.repeatable
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.utils.client.chat
-import net.minecraft.network.packet.s2c.play.PlayerListS2CPacket
 
 object ModulePacketReader : Module("PacketReader", Category.MISC) {
 
     val repeatable = repeatable {
-        val a = player.boundingBox.minY + (player.boundingBox.maxY - player.boundingBox.minY) * 0.5
-        val b = player.boundingBox.maxY * 0.5
-        chat("Shit one $a Shit second $b")
+        val a = arrayListOf(player.boundingBox.minZ, player.boundingBox.minY, player.boundingBox.minZ, player.boundingBox.maxX, player.boundingBox.maxY, player.boundingBox.maxZ)
+        chat(a.toString())
     }
 }
