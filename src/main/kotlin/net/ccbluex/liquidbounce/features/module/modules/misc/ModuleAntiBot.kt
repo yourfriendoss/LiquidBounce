a9c02377b5eb8da94addae69a879daccca7f8129
memsss <aggelos.bilai73@gmail.com>
2021-09-27 13:21:13 +0300

    tests

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleAntiBot.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleAntiBot.kt
@@ -2,33 +2,18 @@
 
 package net.ccbluex.liquidbounce.features.module.modules.misc
 
-import com.google.gson.JsonParser
-import com.mojang.authlib.GameProfile
 import net.ccbluex.liquidbounce.event.NotificationEvent
 import net.ccbluex.liquidbounce.event.PacketEvent
 import net.ccbluex.liquidbounce.event.handler
-import net.ccbluex.liquidbounce.event.repeatable
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
-import net.ccbluex.liquidbounce.utils.client.chat
 import net.ccbluex.liquidbounce.utils.client.notification
-import net.ccbluex.liquidbounce.utils.entity.ping
-import net.minecraft.entity.Entity
-import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.network.packet.s2c.play.PlayerListS2CPacket
-import org.apache.http.client.methods.HttpGet
-import org.apache.http.impl.client.HttpClients
-import org.apache.http.util.EntityUtils
-import java.util.*
 
 
 object ModuleAntiBot : Module("AntiBot", Category.MISC) {
 
-    private var pName: String? = null
-
     private var canCancelEvent = false
-
-    private val uuidNameCache = hashMapOf<UUID, String>()
 
     val packetHandler = handler<PacketEvent> { event ->
 
@@ -41,22 +26,31 @@ object ModuleAntiBot : Module("AntiBot", Category.MISC) {
                                 continue
                             }
 
-                            startChecking(entry.profile.id)
+                            if (isADuplicate(entry.profile.name)) {
+                                canCancelEvent = true
+                                continue
+                            }
 
-                             if (canCancelEvent) {
-                                 event.cancelEvent()
-                                 notification(
-                                     "AntiBot",
-                                     "Removed ${entry.profile.name}",
-                                     NotificationEvent.Severity.INFO
-                                 )
-                                 continue
-                             }
+                            for (i in 0..3) {
+                                if (world.getPlayerByUuid(entry.profile.id)!!.inventory.getArmorStack(i).isEmpty || entry.latency < 2) {
+                                    break
+                                } else {
+                                    canCancelEvent = true
+                                    break
+                                }
+                            }
 
-                             pName = entry.profile.name
+                            if (canCancelEvent) {
+                                event.cancelEvent()
+                                notification(
+                                    "AntiBot",
+                                    "Removed ${entry.profile.name}",
+                                    NotificationEvent.Severity.INFO
+                                )
+                            }
                         }
                     }
-                    PlayerListS2CPacket.Action.REMOVE_PLAYER -> {
+                    /*PlayerListS2CPacket.Action.REMOVE_PLAYER -> {
                         for (entry in packet.entries) {
                             if (entry.profile.name == pName) {
                                 pName = null
@@ -64,7 +58,8 @@ object ModuleAntiBot : Module("AntiBot", Category.MISC) {
                         }
                     }
                     else -> {
-                    }
+                    }*/
+                    else -> { }
                 }
             }
         }
@@ -91,54 +86,29 @@ object ModuleAntiBot : Module("AntiBot", Category.MISC) {
         }
     }*/
 
-    private fun isADuplicate(name: String): Boolean {
+    fun isADuplicate(name: String): Boolean {
         return network.playerList.count { it.profile.name == name } > 0
     }
 
-    private fun isArmored(entity: PlayerEntity): Boolean {
+    /*private fun isArmored(entity: PlayerEntity): Boolean {
         for (i in 0..3) {
             return !entity.inventory.getArmorStack(i).isEmpty
         }
         return false
-    }
+    }*/
 
-    fun getUsername(uuid: UUID): String? {
-        val client = HttpClients.createDefault()
-        val request = HttpGet("https://api.mojang.com/user/profiles/${uuid}/names")
-        val response = client.execute(request)
+    /*   fun getUsername(uuid: UUID): String? {
+           val client = HttpClients.createDefault()
+           val request = HttpGet("https://api.mojang.com/user/profiles/${uuid}/names")
+           val response = client.execute(request)
 
-        if (response.statusLine.statusCode != 200) {
-            return null
-        }
+           if (response.statusLine.statusCode != 200) {
+               return null
+           }
 
-        val names = JsonParser().parse(EntityUtils.toString(response.entity)).asJsonArray
+           val names = JsonParser().parse(EntityUtils.toString(response.entity)).asJsonArray
 
-        return names.get(names.size() - 1).asJsonObject.get("name").asString
-    }
+           return names.get(names.size() - 1).asJsonObject.get("name").asString
+       }*/
 
-
-    fun startChecking(uuid: UUID) {
-        uuidNameCache.clear()
-
-        network.playerUuids.add(uuid)
-
-        for (id in network.playerUuids) {
-            if (id != uuid) {
-                uuidNameCache[id] = getUsername(id)!!
-            }
-        }
-        chat (uuidNameCache.toString())
-
-        if (uuidNameCache.contains(uuid) && uuidNameCache.containsValue(getUsername(uuid))) {
-            chat("$uuid of ${getUsername(uuid)}")
-            for (entity in world.entities) {
-                if (entity is PlayerEntity && entity.entityName == getUsername(uuid)) {
-                    chat("yes??????")
-                    if (isArmored(entity)) {
-                        chat("te")
-                    }
-                }
-            }
-        }
-    }
 }
