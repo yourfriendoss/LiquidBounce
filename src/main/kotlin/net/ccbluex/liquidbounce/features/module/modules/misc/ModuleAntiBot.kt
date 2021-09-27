c85b9226471463ec5ceb295b14b2241639693368
memsss <aggelos.bilai73@gmail.com>
2021-09-27 17:02:28 +0300

    tests

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleAntiBot.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleAntiBot.kt
@@ -6,31 +6,76 @@ import com.mojang.authlib.GameProfile
 import net.ccbluex.liquidbounce.event.NotificationEvent
 import net.ccbluex.liquidbounce.event.PacketEvent
 import net.ccbluex.liquidbounce.event.handler
+import net.ccbluex.liquidbounce.event.repeatable
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.utils.client.notification
+import net.minecraft.entity.Entity
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.network.packet.s2c.play.PlayerListS2CPacket
-import org.apache.http.client.methods.HttpGet
-import org.apache.http.impl.client.HttpClients
 
 object ModuleAntiBot : Module("AntiBot", Category.MISC) {
 
-    val packetHandler = handler<PacketEvent> { event ->
+    private var pName: String? = null
 
+    val packetHandler = handler<PacketEvent> { event ->
         if (mc.world == null || mc.player == null) {
             return@handler
         }
 
-        if (event.packet is PlayerListS2CPacket && event.packet.action == PlayerListS2CPacket.Action.ADD_PLAYER) {
-            for (entry in event.packet.entries) {
-                if (entry.profile.name.length < 3 || entry.latency < 2 || entry.profile.id == player.uuid && entry.profile.name == player.entityName) {
-                    continue
+        when (val packet = event.packet) {
+            is PlayerListS2CPacket -> {
+                when (packet.action) {
+                    PlayerListS2CPacket.Action.ADD_PLAYER -> {
+                        for (entry in packet.entries) {
+                            if (entry.profile.name.length < 3 || entry.latency < 2 || entry.profile.id == player.uuid && entry.profile.name == player.entityName) {
+                                continue
+                            }
+
+                            if (isADuplicate(entry.profile)) {
+                                event.cancelEvent()
+                                notification(
+                                    "AntiBot",
+                                    "Removed ${entry.profile.name}",
+                                    NotificationEvent.Severity.INFO
+                                )
+                                continue
+                            }
+
+                            pName = entry.profile.name
+                        }
+                    }
+                    PlayerListS2CPacket.Action.REMOVE_PLAYER -> {
+                        for (entry in packet.entries) {
+                            if (entry.profile.name == pName) {
+                                pName = null
+                            }
+                        }
+                    }
+                    else -> {
+                    }
+                }
+            }
+        }
+    }
+
+    val repeatable = repeatable {
+        if (pName == null) {
+            return@repeatable
+        }
+
+        for (entity in world.entities) {
+            if (entity is PlayerEntity && entity.entityName == pName) {
+                if (!isArmored(entity)) {
+                    pName = null
                 }
 
-                if (isADuplicate(entry.profile) || !doesSiteAcceptName(entry.profile)) {
-                    event.cancelEvent()
-                    notification("AntiBot", "Removed ${entry.profile.name}", NotificationEvent.Severity.INFO)
+                if (pName != null) {
+                    world.removeEntity(entity.id, Entity.RemovalReason.DISCARDED)
+                    notification("AntiBot", "Removed $pName", NotificationEvent.Severity.INFO)
+                    pName = null
                 }
+                break
             }
         }
     }
@@ -39,12 +84,10 @@ object ModuleAntiBot : Module("AntiBot", Category.MISC) {
         return network.playerList.count { it.profile.name == profile.name } > 0
     }
 
-
-    private fun doesSiteAcceptName(profile: GameProfile): Boolean {
-        val client = HttpClients.createDefault()
-        val request = HttpGet("https://api.mojang.com/user/profiles/${profile.id}/names")
-        val response = client.execute(request)
-
-        return response.statusLine.statusCode == 200
+    private fun isArmored(entity: PlayerEntity): Boolean {
+        for (i in 0..3) {
+            return !entity.inventory.getArmorStack(i).isEmpty
+        }
+        return false
     }
 }
