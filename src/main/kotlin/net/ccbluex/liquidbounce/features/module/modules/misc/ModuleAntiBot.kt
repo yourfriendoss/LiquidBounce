f8e4acd21e92040b6aaa1caafb8d4bb04ec4b11b
memsss <aggelos.bilai73@gmail.com>
2021-09-27 13:33:54 +0300

    tests

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleAntiBot.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleAntiBot.kt
@@ -2,18 +2,18 @@
 
 package net.ccbluex.liquidbounce.features.module.modules.misc
 
+import com.mojang.authlib.GameProfile
 import net.ccbluex.liquidbounce.event.NotificationEvent
 import net.ccbluex.liquidbounce.event.PacketEvent
 import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.utils.client.chat
 import net.ccbluex.liquidbounce.utils.client.notification
 import net.minecraft.network.packet.s2c.play.PlayerListS2CPacket
 
 
 object ModuleAntiBot : Module("AntiBot", Category.MISC) {
-
-    private var canCancelEvent = false
 
     val packetHandler = handler<PacketEvent> { event ->
 
@@ -26,75 +26,39 @@ object ModuleAntiBot : Module("AntiBot", Category.MISC) {
                                 continue
                             }
 
-                            if (isADuplicate(entry.profile.name)) {
-                                canCancelEvent = true
-                            }
-
-                            for (i in 0..3) {
-                                if (world.getPlayerByUuid(entry.profile.id)!!.inventory.getArmorStack(i).isEmpty || entry.latency < 2) {
-                                    break
-                                } else {
-                                    canCancelEvent = true
-                                    break
-                                }
-                            }
-
-                            if (canCancelEvent) {
+                            if (isADuplicate(entry.profile)) {
                                 event.cancelEvent()
                                 notification(
                                     "AntiBot",
                                     "Removed ${entry.profile.name}",
                                     NotificationEvent.Severity.INFO
                                 )
+                                chat("duplicate, whatn andiot")
+                            } else {
+                                chat("oh u entered")
+                                for (i in 0..3) {
+                                    if (!world.getPlayerByUuid(entry.profile.id)!!.inventory.getArmorStack(i).isEmpty && entry.latency > 1) {
+                                        chat("u also entered this, cool cool")
+                                        event.cancelEvent()
+                                        notification(
+                                            "AntiBot",
+                                            "Removed ${entry.profile.name}",
+                                            NotificationEvent.Severity.INFO
+                                        )
+                                    }
+                                }
                             }
                         }
                     }
-                    /*PlayerListS2CPacket.Action.REMOVE_PLAYER -> {
-                        for (entry in packet.entries) {
-                            if (entry.profile.name == pName) {
-                                pName = null
-                            }
-                        }
-                    }
-                    else -> {
-                    }*/
                     else -> { }
                 }
             }
         }
     }
 
-/*    val repeatable = repeatable {
-        if (pName == null) {
-            return@repeatable
-        }
-
-        for (entity in world.entities) {
-            if (entity is PlayerEntity && entity.entityName == pName) {
-                if (!isArmored(entity) || entity.ping < 2) {
-                    pName = null
-                }
-
-                if (pName != null) {
-                    world.removeEntity(entity.id, Entity.RemovalReason.DISCARDED)
-                    notification("AntiBot", "Removed $pName", NotificationEvent.Severity.INFO)
-                    pName = null
-                }
-                break
-            }
-        }
-    }*/
-
-    fun isADuplicate(name: String): Boolean {
-        return network.playerList.count { it.profile.name == name } > 0
+    private fun isADuplicate(profile: GameProfile): Boolean {
+        return network.playerList.count { it.profile.name == profile.name } > 0
     }
-
-    /*private fun isArmored(entity: PlayerEntity): Boolean {
-        for (i in 0..3) {
-            return !entity.inventory.getArmorStack(i).isEmpty
-        }
-        return false
-    }*/
 
     /*   fun getUsername(uuid: UUID): String? {
            val client = HttpClients.createDefault()
