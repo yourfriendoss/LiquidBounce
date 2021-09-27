65fec17732c3505994ebecd79bee74206c9ed1d0
memsss <aggelos.bilai73@gmail.com>
2021-09-27 14:05:57 +0300

    tests

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModulePacketReader.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModulePacketReader.kt
@@ -1,16 +1,16 @@
 package net.ccbluex.liquidbounce.features.module.modules.misc
 
 import net.ccbluex.liquidbounce.event.PacketEvent
-import net.ccbluex.liquidbounce.event.TransferOrigin
 import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.utils.client.chat
+import net.minecraft.network.packet.s2c.play.PlayerListS2CPacket
 
 object ModulePacketReader : Module("PacketReader", Category.MISC) {
 
     val p = handler<PacketEvent> { event ->
-        if (event.origin == TransferOrigin.RECEIVE) {
+        if (event.packet is PlayerListS2CPacket && event.packet.action == PlayerListS2CPacket.Action.ADD_PLAYER) {
             chat(event.packet.toString())
         }
     }
