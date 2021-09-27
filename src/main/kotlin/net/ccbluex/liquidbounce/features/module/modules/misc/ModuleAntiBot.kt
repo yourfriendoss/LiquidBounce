6ebd7642493881c786119a351c8948b6903361e0
memsss <aggelos.bilai73@gmail.com>
2021-09-27 14:25:58 +0300

    tests

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleAntiBot.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleAntiBot.kt
@@ -8,45 +8,22 @@ import net.ccbluex.liquidbounce.event.PacketEvent
 import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
-import net.ccbluex.liquidbounce.utils.client.chat
 import net.ccbluex.liquidbounce.utils.client.notification
-import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.network.packet.s2c.play.PlayerListS2CPacket
 
 
 object ModuleAntiBot : Module("AntiBot", Category.MISC) {
 
     val packetHandler = handler<PacketEvent> { event ->
+        if (event.packet is PlayerListS2CPacket && event.packet.action == PlayerListS2CPacket.Action.ADD_PLAYER) {
+            for (entry in event.packet.entries) {
+                if (entry.profile.name.length < 3) {
+                    continue
+                }
 
-        when (val packet = event.packet) {
-            is PlayerListS2CPacket -> {
-                when (packet.action) {
-                    PlayerListS2CPacket.Action.ADD_PLAYER -> {
-                        for (entry in packet.entries) {
-                            if (entry.profile.name.length < 3) {
-                                continue
-                            }
-
-                            if (!(entry.profile as PlayerEntity).inventory.getArmorStack(1).isEmpty) {
-                                chat("not empty lol")
-                            }
-
-                            if (world.getPlayerByUuid(entry.profile.id)!!.inventory.getArmorStack(1).isEmpty) {
-                                chat("met[pyepy")
-                            }
-
-                            if (isADuplicate(entry.profile)) {
-                                event.cancelEvent()
-                                notification(
-                                    "AntiBot",
-                                    "Removed ${entry.profile.name}",
-                                    NotificationEvent.Severity.INFO
-                                )
-                                chat("duplicate, whatn andiot")
-                            }
-                        }
-                    }
-                    else -> { }
+                if (isADuplicate(entry.profile) || (entry.profile == null && entry.latency > 1)) {
+                    event.cancelEvent()
+                    notification("AntiBot", "Removed ${entry.profile.name}", NotificationEvent.Severity.INFO)
                 }
             }
         }
@@ -55,19 +32,5 @@ object ModuleAntiBot : Module("AntiBot", Category.MISC) {
     private fun isADuplicate(profile: GameProfile): Boolean {
         return network.playerList.count { it.profile.name == profile.name } > 0
     }
-
-    /*   fun getUsername(uuid: UUID): String? {
-           val client = HttpClients.createDefault()
-           val request = HttpGet("https://api.mojang.com/user/profiles/${uuid}/names")
-           val response = client.execute(request)
-
-           if (response.statusLine.statusCode != 200) {
-               return null
-           }
-
-           val names = JsonParser().parse(EntityUtils.toString(response.entity)).asJsonArray
-
-           return names.get(names.size() - 1).asJsonObject.get("name").asString
-       }*/
 
 }
