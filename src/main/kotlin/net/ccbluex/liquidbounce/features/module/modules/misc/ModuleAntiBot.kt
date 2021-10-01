d64a89390e3a4e49dcb5153cd60754f2b34dec9d
memsss <aggelos.bilai73@gmail.com>
2021-10-01 22:06:20 +0300

    tests

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleAntiBot.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleAntiBot.kt
@@ -21,7 +21,7 @@ object ModuleAntiBot : Module("AntiBot", Category.MISC) {
     val packetHandler = handler<PacketEvent> { event ->
         if (event.packet is PlayerListS2CPacket && event.packet.action == PlayerListS2CPacket.Action.ADD_PLAYER) {
             for (entry in event.packet.entries) {
-                if (entry.latency < 2 || entry.profile.name.length < 3 || !entry.profile.properties.isEmpty) {
+                if (entry.latency < 2 || entry.profile.name.length < 3 || !entry.profile.properties.isEmpty || isTheSamePlayer(entry.profile)) {
                     continue
                 }
 
@@ -63,6 +63,12 @@ object ModuleAntiBot : Module("AntiBot", Category.MISC) {
         for (i in 0..3) {
             return !entity.inventory.getArmorStack(i).isEmpty
         }
+        return false
+    }
+
+    private fun isTheSamePlayer(profile: GameProfile): Boolean {
+        // Prevents false positives when a player joins a Mini game
+        network.playerList.forEach { return it.profile.name == profile.name && it.profile.id == profile.id }
         return false
     }
 
