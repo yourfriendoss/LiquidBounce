8c397e3a2e78dec6bca58c58363fafa8f22f154e
memsss <aggelos.bilai73@gmail.com>
2021-10-14 20:48:02 +0300

    test

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/ModuleTeleportAura.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/ModuleTeleportAura.kt
@@ -5,8 +5,6 @@ import net.ccbluex.liquidbounce.event.EventManager
 import net.ccbluex.liquidbounce.event.repeatable
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
-import net.ccbluex.liquidbounce.features.module.modules.combat.ModuleKillAura.scanExtraRange
-import net.ccbluex.liquidbounce.utils.aiming.Rotation
 import net.ccbluex.liquidbounce.utils.aiming.RotationManager
 import net.ccbluex.liquidbounce.utils.client.MC_1_8
 import net.ccbluex.liquidbounce.utils.client.protocolVersion
@@ -23,7 +21,7 @@ import net.minecraft.util.Hand
 
 object ModuleTeleportAura : Module("TeleportAura", Category.COMBAT) {
 
-    private val range by float("Range", 100f, (ModuleKillAura.range + scanExtraRange)..100f)
+    private val range by float("Range", 100f, 8f..100f)
     private val timer by float("Timer", 1f, 0.1f..2f)
     private val steps by float("Steps", 4f, 2f..10f)
     private val swing by boolean("Swing", true)
@@ -48,36 +46,34 @@ object ModuleTeleportAura : Module("TeleportAura", Category.COMBAT) {
         }
 
         val rangeSquared = range * range
-        val killAuraRange = ((ModuleKillAura.range + scanExtraRange) * (ModuleKillAura.range + scanExtraRange))
 
         for (enemy in targetTracker.enemies()) {
-            if (enemy.squaredBoxedDistanceTo(player) > killAuraRange && enemy.squaredBoxedDistanceTo(player) <= rangeSquared) {
-                targetTracker.lock(enemy)
-                val (rotation, _) = RotationManager.raytraceBox(
-                    player.eyesPos,
-                    enemy.boundingBox,
-                    range = range.toDouble(),
-                    wallsRange = 0.0
-                ) ?: continue
-
-                val vec = Rotation(player.yaw, 0f).rotationVec
-                val x = player.x + vec.x * player.distanceTo(enemy) - 1f
-                val y = enemy.y + 0.25
-                val z = player.z + vec.z * player.distanceTo(enemy) - 1f
-                findPath(x, y + 1.0, z, steps.toDouble()).forEach { pos ->
-                    network.sendPacket(
-                        PlayerMoveC2SPacket.Full(pos.x, pos.y, pos.z, rotation.yaw, rotation.pitch, false)
-                    )
-                }
-                if (player.isOnGround) {
-                    player.jump()
-                }
-                mc.timer.timerSpeed = timer
-                resetTimer = true
-                attack(enemy)
+            if (enemy.squaredBoxedDistanceTo(player) > rangeSquared) {
+                continue
             }
+
+            val (rotation, _) = RotationManager.raytraceBox(
+                player.eyesPos,
+                enemy.boundingBox,
+                range = range.toDouble(),
+                wallsRange = range.toDouble()
+            ) ?: continue
+
+            findPath(enemy.x, enemy.y + 1.0, enemy.z, steps.toDouble()).forEach { pos ->
+                network.sendPacket(
+                    PlayerMoveC2SPacket.Full(pos.x, pos.y, pos.z, rotation.yaw, rotation.pitch, false)
+                )
+            }
+
+            if (player.isOnGround) {
+                player.jump()
+            }
+            mc.timer.timerSpeed = timer
+            resetTimer = true
+            attack(enemy)
         }
     }
+
 
     fun attack(entity: Entity) {
         EventManager.callEvent(AttackEvent(entity))
