21f5f0bc67aedd6e56ed682e1daa89ca4b6bb7cb
memsss <aggelos.bilai73@gmail.com>
2021-10-14 22:16:42 +0300

    test so called infinite aura

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/ModuleTeleportAura.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/ModuleTeleportAura.kt
@@ -1,33 +1,43 @@
 package net.ccbluex.liquidbounce.features.module.modules.combat
 
-import net.ccbluex.liquidbounce.event.AttackEvent
-import net.ccbluex.liquidbounce.event.EventManager
-import net.ccbluex.liquidbounce.event.repeatable
+import net.ccbluex.liquidbounce.event.*
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.render.engine.Color4b
+import net.ccbluex.liquidbounce.render.engine.RenderEngine
+import net.ccbluex.liquidbounce.render.engine.Vec3
+import net.ccbluex.liquidbounce.render.engine.memory.PositionColorVertexFormat
+import net.ccbluex.liquidbounce.render.engine.memory.putVertex
+import net.ccbluex.liquidbounce.render.utils.drawBoxNew
+import net.ccbluex.liquidbounce.render.utils.drawBoxOutlineNew
 import net.ccbluex.liquidbounce.utils.aiming.Rotation
-import net.ccbluex.liquidbounce.utils.aiming.RotationManager
+import net.ccbluex.liquidbounce.utils.aiming.facingEnemy
 import net.ccbluex.liquidbounce.utils.client.MC_1_8
 import net.ccbluex.liquidbounce.utils.client.protocolVersion
 import net.ccbluex.liquidbounce.utils.client.timer
 import net.ccbluex.liquidbounce.utils.combat.TargetTracker
-import net.ccbluex.liquidbounce.utils.entity.eyesPos
 import net.ccbluex.liquidbounce.utils.entity.squaredBoxedDistanceTo
 import net.ccbluex.liquidbounce.utils.path.findPath
+import net.ccbluex.liquidbounce.utils.render.espBoxInstancedOutlineRenderTask
+import net.ccbluex.liquidbounce.utils.render.espBoxInstancedRenderTask
 import net.minecraft.entity.Entity
 import net.minecraft.network.packet.c2s.play.HandSwingC2SPacket
 import net.minecraft.network.packet.c2s.play.PlayerInteractEntityC2SPacket
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket
 import net.minecraft.util.Hand
+import net.minecraft.util.math.Box
+import net.minecraft.util.math.Vec3d
 
-object ModuleTeleportAura : Module("TeleportAura", Category.COMBAT) {
+object ModuleTeleportAura : Module("TeleportHit", Category.COMBAT) {
 
+    private val swing by boolean("Swing", true)
     private val range by float("Range", 100f, 8f..100f)
     private val timer by float("Timer", 1f, 0.1f..2f)
-    private val steps by float("Steps", 4f, 2f..10f)
-    private val swing by boolean("Swing", true)
+    private val steps by int("Steps", 4, 2..10)
 
     private val targetTracker = tree(TargetTracker())
+
+    private var positions = ArrayList<Vec3d>()
 
     private var resetTimer = false
 
@@ -53,30 +63,46 @@ object ModuleTeleportAura : Module("TeleportAura", Category.COMBAT) {
                 continue
             }
 
-            val (rotation, _) = RotationManager.raytraceBox(
-                player.eyesPos,
-                enemy.boundingBox,
-                range = range.toDouble(),
-                wallsRange = range.toDouble()
-            ) ?: continue
-
-            val vec = Rotation(player.yaw, 0f).rotationVec
-            val x = player.x + vec.x * player.distanceTo(enemy) - 1f
-            val y = enemy.y + 0.25
-            val z = player.z + vec.z * player.distanceTo(enemy) - 1f
-
-            findPath(x, y + 1.0, z, steps.toDouble()).forEach { pos ->
-                network.sendPacket(
-                    PlayerMoveC2SPacket.PositionAndOnGround(pos.x, pos.y, pos.z, false)
-                )
+            if (facingEnemy(enemy, range.toDouble(), Rotation(player.yaw, player.pitch)) && mc.options.keyAttack.isPressed) {
+                findPath(enemy.x, enemy.y + 1, enemy.z, steps.toDouble()).forEach { pos ->
+                    network.sendPacket(
+                        PlayerMoveC2SPacket.Full(pos.x, pos.y, pos.z, player.yaw, player.pitch, false)
+                    )
+                    positions.add(pos)
+                }
+                mc.timer.timerSpeed = timer
+                resetTimer = true
+                attack(enemy)
             }
-
-            mc.timer.timerSpeed = timer
-            resetTimer = true
-            attack(enemy)
         }
     }
 
+    val box = drawBoxNew(Box(0.0, 0.0, 0.0, 1.0, 1.0, 1.0), Color4b.WHITE)
+
+    val boxOutline = drawBoxOutlineNew(Box(0.0, 0.0, 0.0, 1.0, 1.0, 1.0), Color4b.WHITE)
+
+    val tickHandler = handler<EngineRenderEvent> {
+        val instanceBuffer = PositionColorVertexFormat()
+        val instanceBufferOutline = PositionColorVertexFormat()
+        for (i in positions) {
+
+            instanceBuffer.initBuffer(positions.size)
+            instanceBufferOutline.initBuffer(positions.size)
+
+            instanceBuffer.putVertex { this.position = Vec3(i); this.color = Color4b.WHITE }
+            instanceBufferOutline.putVertex { this.position = Vec3(i); this.color = Color4b.WHITE }
+        }
+
+        RenderEngine.enqueueForRendering(
+        RenderEngine.CAMERA_VIEW_LAYER,
+        espBoxInstancedRenderTask(instanceBuffer, box.first, box.second)
+        )
+        RenderEngine.enqueueForRendering(
+        RenderEngine.CAMERA_VIEW_LAYER,
+        espBoxInstancedOutlineRenderTask(instanceBufferOutline, boxOutline.first, boxOutline.second)
+        )
+
+    }
 
     fun attack(entity: Entity) {
         EventManager.callEvent(AttackEvent(entity))
