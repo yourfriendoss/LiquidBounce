d496630bdd4331913cebe9f01a672e48a684755a
mems01 <blueenderpal.bilai@gmail.com>
2021-10-18 19:21:12 +0300

    enderpearl tests

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/ModuleFly.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/ModuleFly.kt
@@ -168,14 +168,13 @@ object ModuleFly : Module("Fly", Category.MOVEMENT) {
         }
 
         val packetHandler = handler<PacketEvent> { event ->
-            if (event.packet is PlaySoundS2CPacket &&
-                event.packet.sound == SoundEvents.ENTITY_ENDER_PEARL_THROW &&
-                event.packet is TeleportConfirmC2SPacket && threwPearl
-            ) {
-                chat("hi!!!! yES!")
-                canFly = true
+            if (event.packet is PlaySoundS2CPacket && event.packet.sound == SoundEvents.ENTITY_ENDER_PEARL_THROW) {
+                chat("yes!!! PART 1")
+                if (event.packet is TeleportConfirmC2SPacket && threwPearl) {
+                    chat("hi!!!! yES!")
+                    canFly = true
+                }
             }
         }
     }
-
 }
