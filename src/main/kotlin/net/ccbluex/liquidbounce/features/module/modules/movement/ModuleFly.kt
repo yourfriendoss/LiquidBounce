fa194cd0f5f112a70756cd563cec6b29963f554a
mems01 <aggelos.bilai73@gmail.com>
2021-10-19 14:47:15 +0300

    test

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/ModuleFly.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/ModuleFly.kt
@@ -187,7 +187,7 @@ object ModuleFly : Module("Fly", Category.MOVEMENT) {
         }
 
         val packetHandler = handler<PacketEvent> { event ->
-            if ((event.packet is PlaySoundS2CPacket && event.packet.sound == SoundEvents.ENTITY_ENDER_PEARL_THROW) || event.packet is PlayerMoveC2SPacket.Full && threwPearl) {
+            if (event.packet is PlayerMoveC2SPacket.Full || (event.packet is PlaySoundS2CPacket && event.packet.sound == SoundEvents.ENTITY_ENDER_PEARL_THROW) && threwPearl) {
                 canFly = true
             }
         }
