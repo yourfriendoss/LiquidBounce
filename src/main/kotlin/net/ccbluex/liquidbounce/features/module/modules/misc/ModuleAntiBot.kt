9e838f788a6d2a9b9be3a61d6bd794f56ae8ded8
mems01 <blueenderpal.bilai@gmail.com>
2021-10-20 19:46:46 +0300

    tests

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleAntiBot.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleAntiBot.kt
@@ -48,14 +48,6 @@ object ModuleAntiBot : Module("AntiBot", Category.MISC) {
         for (entity in world.entities) {
             if (entity is PlayerEntity && entity.entityName == pName) {
                 chat("${entity.isOnGround} or ${entity.noClip}, or ${entity.isInsideWall} or ${entity.lastAttackTime} or ${entity.lastAttackedTicks} ")
-                if (!isArmored(entity)) {
-                    pName = null
-                    continue
-                }
-
-                world.removeEntity(entity.id, Entity.RemovalReason.DISCARDED)
-                notification("AntiBot", "Removed bot $pName", NotificationEvent.Severity.INFO)
-                pName = null
             }
         }
     }
